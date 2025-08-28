82babedf6495e30f1b9d116cfc062b3721fee72b
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-08-27 22:35:32 -0300

    feat: implementar futuros métodos e ideias iniciais

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 using namespace std;
 
+struct Produto {
+    string nomeProduto;
+    float quantidadeProduto;
+    float valorProduto;
+    
+};
+
+struct Estoque {
+    
+
+
+    void cadastrarProduto(Produto produto) {
+        
+    }
+
+    string listarProdutos() {
+
+    } 
+
+};
+
+
+void menuPrincipal() {
+    
+}
+
 int main() {
+    
+
     
 }
