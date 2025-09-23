953cd171e3d3c4bcf4bec746881e5ce1b7db98c2
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-09-23 12:36:44 -0300

    feat: implementar listagem de produtos e cadastro de produtos

--- main.cpp
+++ main.cpp
@@ -1,24 +1,64 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iomanip>
 using namespace std;
 
+string nomeArquivoAtual = "../produtos.csv";
+
+
 struct Produto {
+    long id; 
     string nomeProduto;
     float quantidadeProduto;
     float valorProduto;
-    
+
 };
 
 struct Estoque {
-    
-
 
     void cadastrarProduto(Produto produto) {
-        
+        ofstream arquivo(nomeArquivoAtual, ios::app);
+
+        arquivo << "\n";
+        arquivo << produto.id << ',' << produto.nomeProduto << ',' << produto.quantidadeProduto << ',' << produto.valorProduto;
     }
 
-    string listarProdutos() {
+    void listarProdutos(string nomeArquivo) {
+        ifstream arquivo(nomeArquivo);
+        string linha;
 
-    } 
+        while (getline(arquivo, linha)) {
+            stringstream ss(linha);
+            string palavra;
+
+            int coluna = 0;
+            while (getline(ss, palavra, ',')) {
+                switch (coluna) {
+                case 0:
+                    cout << left << setw(9) << palavra; 
+                    coluna++;
+                    break;
+                case 1:
+                    cout << left << setw(25) << palavra; 
+                    coluna++;
+                    break;
+                case 2:
+                    cout << left << setw(8) << palavra; 
+                    coluna++;
+                    break;
+                default:
+                    cout << "R$";
+                    cout << left << setw(15) << palavra; 
+                    break;
+                }  
+            }
+            cout << endl;
+        }
+
+        arquivo.close();
+    }
 
 };
 
@@ -28,7 +68,15 @@ void menuPrincipal() {
 }
 
 int main() {
-    
+    Estoque estoque;
+    Produto produto;
+    Produto produto2;
 
+    produto.nomeProduto = "Suco de laranja";
+    produto.quantidadeProduto = 10;
+    produto.valorProduto = 24.2;
+    
+    estoque.cadastrarProduto(produto);
+    estoque.listarProdutos(nomeArquivoAtual);
     
 }
