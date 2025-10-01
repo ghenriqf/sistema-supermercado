188886804a587f19e5b0bf95c50a0f0872b2f73f
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-10-01 16:22:30 -0300

    refactor: melhorar visibilidade do codigo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip>
 #include <fstream>
 #include <sstream>
 #include <string>
@@ -10,15 +9,13 @@
 
 using namespace std;
 
-string nomeArquivoAtual = "../produtos.csv";
-
 struct Produto {
-    int id; 
+    int id;
     string nomeProduto;
     float quantidadeProduto;
     float valorProduto;
 
-    Produto () {
+    Produto() {
         id = rand();
     }
 };
@@ -27,28 +24,23 @@ struct Administrador {
 
     string nomeUsuario;
     string senha;
-    string confSenha;
-    
 
     void cadastrar(Administrador adm) {
-        if (senha == confSenha){
-            ofstream arquivo("../adms.csv", ios::app);
-    
-            if (arquivo.is_open()) {
-                arquivo << adm.nomeUsuario << ',' << adm.senha << "\n";
-                arquivo.close();
-            } else {
-                throw runtime_error("ERRO: Nao foi possivel abrir o arquivo");
-            }
-        } else {
-            throw runtime_error ("As senhas nao coincidem!");
+        ofstream arquivo("../adms.csv", ios::app);
+
+        if (arquivo.is_open()) {
+            arquivo << adm.nomeUsuario << ',' << adm.senha << "\n";
+            arquivo.close();
+        }
+        else {
+            throw runtime_error("ERRO: Nao foi possivel abrir o arquivo");
         }
     }
-
-
 };
 
 struct Estoque {
+
+    string nomeArquivoAtual;
 
     Produto buscarProduto(int id) {
         ifstream estoque(nomeArquivoAtual);
@@ -59,7 +51,7 @@ struct Estoque {
 
         string linha;
         Produto produto;
-        
+
         while (getline(estoque, linha)) {
             stringstream ss(linha);
 
@@ -82,51 +74,50 @@ struct Estoque {
         }
 
         throw runtime_error("ERRO: Produto nao encontrado!");
-        }
+        estoque.close();
+    }
 
     void atualizarProduto(Produto atualizado) {
 
         ofstream temp("../temp.csv");
         ifstream arquivo(nomeArquivoAtual);
-        
+
         if (!arquivo.is_open() || !temp.is_open()) {
             throw runtime_error("ERRO: Nao foi possível abrir o arquivo!");
         }
-        
+
         string linha;
 
-        while (getline(arquivo,linha)) {
-
+        while (getline(arquivo, linha)) {
             string id;
 
             stringstream ss(linha);
-            getline(ss,id,',');
+            getline(ss, id, ',');
 
             if (id == to_string(atualizado.id)) {
                 temp << atualizado.id << ',' << atualizado.nomeProduto << ',' << atualizado.quantidadeProduto << ',' << atualizado.valorProduto << endl;
-            } else {
-                 temp << linha << endl;
             }
-            
+            else {
+                temp << linha << endl;
+            }
         }
-        
+
         arquivo.close();
         temp.close();
 
         remove(nomeArquivoAtual.c_str());
         rename("../temp.csv", nomeArquivoAtual.c_str());
-
     }
 
     void cadastrarProduto(Produto produto) {
         ofstream arquivo(nomeArquivoAtual, ios::app);
 
         if (arquivo.is_open()) {
-            arquivo << produto.id << ',' << produto.nomeProduto << ',' << produto.quantidadeProduto << ',' << produto.valorProduto << "\n" ;
-        } else {
-              throw runtime_error("ERRO: Nao foi possivel abrir o arquivo!");
+            arquivo << produto.id << ',' << produto.nomeProduto << ',' << produto.quantidadeProduto << ',' << produto.valorProduto << "\n";
         }
-        
+        else {
+            throw runtime_error("ERRO: Nao foi possivel abrir o arquivo!");
+        }
     }
 
     void listarProdutos(string nomeArquivo) {
@@ -142,37 +133,34 @@ struct Estoque {
                 while (getline(ss, palavra, ',')) {
                     switch (coluna) {
                     case 0:
-                        cout << left << setw(9) << palavra; 
+                        cout << left << setw(9) << palavra;
                         coluna++;
                         break;
                     case 1:
-                        cout << left << setw(25) << palavra; 
+                        cout << left << setw(25) << palavra;
                         coluna++;
                         break;
                     case 2:
-                        cout << left << setw(8) << palavra; 
+                        cout << left << setw(8) << palavra;
                         coluna++;
                         break;
                     case 3:
                         cout << "R$";
-                        cout << left << setw(15) << palavra; 
+                        cout << left << setw(15) << palavra;
                         break;
-                }  
-            } 
-            
-            cout << endl;
+                    }
+                }
 
-            } 
-        } 
+                cout << endl;
+            }
+        }
         else {
-              throw runtime_error("ERRO: Nao foi possivel abrir o arquivo");
+            throw runtime_error("ERRO: Nao foi possivel abrir o arquivo");
         }
         arquivo.close();
     }
-
 };
 
 int main() {
     srand(time(0));
-
 }
